acb15c73a75ca1b410778afcb5a37e8995542eb5
Chris Nyaga <nyagachris75@gmail.com>
2017-06-19 17:08:10 +0300

    Ride model updated

--- app/models/ride.js
+++ app/models/ride.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  rider: DS.belongsTo('rider'),
+  user: DS.belongsTo('user'),
+  startingPoint: DS.attr(),
+  endingTo: DS.attr(),
+  amountCharged: DS.attr()
 });
