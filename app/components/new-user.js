b7cf920fdf22eeb17fafbda1931ae14690f39a4b
Chris Nyaga <nyagachris75@gmail.com>
2017-06-20 10:00:49 +0300

    New user now picked from form and action sent upwards

--- app/components/new-user.js
+++ app/components/new-user.js
@@ -2,4 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   session: Ember.inject.service(),
+  actions:{
+    newUser(){
+      var params = {
+        id: this.get('id'),
+        name: this.get('name'),
+        email: this.get('session.data.authenticated.profile.email')
+      };
+      this.sendAction('registerNewUser',params),
+      this.set('id',''),
+      this.set('names','')
+    }
+  }
 });
