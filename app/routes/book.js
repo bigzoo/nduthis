fabd0bba73338efe69940f7f6c9a8807984c827b
Chris Nyaga <nyagachris75@gmail.com>
2017-06-20 15:01:39 +0300

    Book route model to only show available riders

--- app/routes/book.js
+++ app/routes/book.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin,{
-  model(){
-    return this.store.findAll('rider')
-  }
+  model: function() {
+     return this.store.query('rider', {
+       orderBy: 'status',
+       equalTo: 'available'
+     });
+   }
 });
