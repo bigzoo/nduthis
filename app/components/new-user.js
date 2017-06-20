c4f1c845e99c362ff61de6fa624f13995e85e0fd
Chris Nyaga <nyagachris75@gmail.com>
2017-06-20 10:20:02 +0300

    New users are now registered and other form tools have been added such as clearing input

--- app/components/new-user.js
+++ app/components/new-user.js
@@ -1,17 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewUser: false,
   session: Ember.inject.service(),
   actions:{
+    addNewUser(){
+      this.set('addNewUser',true)
+    },
+    resetForm(){
+      this.set('id',''),
+      this.set('name','')
+    },
+    cancelForm(){
+      this.set('addNewUser',false),
+      this.set('id',''),
+      this.set('name','')
+    },
     newUser(){
       var params = {
-        id: this.get('id'),
+        idNumber: this.get('id'),
         name: this.get('name'),
         email: this.get('session.data.authenticated.profile.email')
       };
       this.sendAction('registerNewUser',params),
       this.set('id',''),
-      this.set('names','')
+      this.set('name',''),
+      this.set('addNewUser',false)
     }
   }
 });
