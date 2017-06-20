85ce991a880023d74d597238a7ad806465a3cfda
Chris Nyaga <nyagachris75@gmail.com>
2017-06-20 14:56:10 +0300

    Nested route rider now appears in modal when clicked

---
+++ app/controllers/book/rider.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isShowingModal: true,
+  actions:{
+    closeModalDialog(){
+      this.set('isShowingModal', false)
+    }
+  }
+});
