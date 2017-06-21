6bdeab4b8bb26520bcbaee167682f25a9e7f39fb
Chris Nyaga <nyagachris75@gmail.com>
2017-06-21 12:52:14 +0300

    Users dashboard displays all the users rides

--- app/routes/dashboard/user.js
+++ app/routes/dashboard/user.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  session: Ember.inject.service(),
+  model() {
+    return Ember.RSVP.hash({
+      user: this.store.query('user', {
+        orderBy: 'email',
+        equalTo: this.get('session.data.authenticated.profile.email')
+      })
+    })
+  }
 });
