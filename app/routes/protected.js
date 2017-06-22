09e2a4e97c05934a290395072a0622b39ba86119
Chris Nyaga <nyagachris75@gmail.com>
2017-06-22 16:49:37 +0300

    Unregistered users are sent to the registration page after verifying their account

--- app/routes/protected.js
+++ app/routes/protected.js
@@ -6,22 +6,31 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   check: Ember.inject.service(),
   model() {
     var checkU = this.get('check');
+    var session = this.get('session.data.authenticated.profile.email');
     return this.store.query('user', {
-      sortBy: 'email',
-      equalTo: this.get('session.data.aduthenticated.profile.email')
+      orderBy: 'email',
+      equalTo: this.get('session.data.authenticated.profile.email')
     }).then(function(user) {
-      if (user.get('length') === 3) {
-        Ember.set(checkU, 'user', 'not found');
-      } else if (user.get('length') === 0) {
+      console.log(user);
+      user.forEach(function (userU) {
+        console.log(userU);
+      })
+      if (user.get('length') === 1) {
         Ember.set(checkU, 'user', 'user');
+      } else {
+        Ember.set(checkU, 'user', 'Not a user. To be determined by Register route.');
       }
-      console.log(user.get('length'));
+      // console.log(session);
+      // console.log(user.get('length'));
+      // console.log(checkU.user);
     })
   },
   afterModel() {
     var checkU = this.get('check');
-    console.log(checkU.user);
-    if (checkU.user === 'not found') {
+    // console.log(checkU.user);
+    if (checkU.user === 'user') {
+      this.transitionTo('dashboard.user');
+    }else {
       this.transitionTo('register');
     }
   }
