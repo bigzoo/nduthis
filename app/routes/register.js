0ef9cae51ee8843225ffc471dd5689a4ca1cafa1
Chris Nyaga <nyagachris75@gmail.com>
2017-06-22 17:40:55 +0300

    Register controller created and session injected. Other changes also.

--- app/routes/register.js
+++ app/routes/register.js
@@ -5,10 +5,12 @@ export default Ember.Route.extend({
     registerNewUser(params){
       var newUser = this.store.createRecord('user', params);
       newUser.save();
+      this.transitionTo('protected');
     },
     registerNewRider(params){
       var newRider = this.store.createRecord('rider', params);
       newRider.save();
+      this.transitionTo('protected');
     }
   }
 });
