addb9030d5ce0cbc6a6d7f70a34628fbaf10d346
Chris Nyaga <nyagachris75@gmail.com>
2017-06-22 14:36:29 +0300

    User is redirected to the register route if he is not already registered

--- app/routes/protected.js
+++ app/routes/protected.js
@@ -1,4 +1,28 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin);
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  session: Ember.inject.service(),
+  check: Ember.inject.service(),
+  model() {
+    var checkU = this.get('check');
+    return this.store.query('user', {
+      sortBy: 'email',
+      equalTo: this.get('session.data.aduthenticated.profile.email')
+    }).then(function(user) {
+      if (user.get('length') === 3) {
+        Ember.set(checkU, 'user', 'not found');
+      } else if (user.get('length') === 0) {
+        Ember.set(checkU, 'user', 'user');
+      }
+      console.log(user.get('length'));
+    })
+  },
+  afterModel() {
+    var checkU = this.get('check');
+    console.log(checkU.user);
+    if (checkU.user === 'not found') {
+      this.transitionTo('register');
+    }
+  }
+});
--- app/services/check.js
+++ app/services/check.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  user:'undefined',
+  session : Ember.inject.service(),
 });
