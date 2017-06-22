7b62972c64609303a77cd0a6dffb3f8a5cf711d6
Chris Nyaga <nyagachris75@gmail.com>
2017-06-22 17:25:58 +0300

    Riders are now redirected to their dashboard immediately they log in.

--- app/routes/protected.js
+++ app/routes/protected.js
@@ -11,27 +11,14 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
       orderBy: 'email',
       equalTo: this.get('session.data.authenticated.profile.email')
     }).then(function(user) {
-      console.log(user);
-      user.forEach(function (userU) {
-        console.log(userU);
-      })
       if (user.get('length') === 1) {
         Ember.set(checkU, 'user', 'user');
       } else {
         Ember.set(checkU, 'user', 'Not a user. To be determined by Register route.');
       }
-      // console.log(session);
-      // console.log(user.get('length'));
-      // console.log(checkU.user);
     })
   },
   afterModel() {
-    var checkU = this.get('check');
-    // console.log(checkU.user);
-    if (checkU.user === 'user') {
-      this.transitionTo('dashboard.user');
-    }else {
-      this.transitionTo('register');
-    }
+    this.transitionTo('dashboard');
   }
 });
--- app/services/check.js
+++ app/services/check.js
@@ -2,5 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   user:'undefined',
+  rider:'undefined',
   session : Ember.inject.service(),
 });
